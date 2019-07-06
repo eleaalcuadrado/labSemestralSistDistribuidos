bb85cdce8939a150a52ae7f02d3103a2932b0c0c
eleaalcuadrado <luis.aguilar@usach.cl>
2019-07-06 18:11:11 -0400

    View conected to api rest

--- front_lab3/lab3/views.py
+++ front_lab3/lab3/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+import urllib.request
+import json
+
+
+
+
 
 # Create your views here.
 def home(request):
@@ -6,8 +12,30 @@ def home(request):
 
 
 def search(request):
+	#dato que se puso en el buscador
 	query = request.GET.get('q')
-	data = [["ceremony1","2000","category1","outcome1","lang1"],["ceremony2","2000","category2","outcome2","lang2"]]
+
+	#obtener json desde API
+	url = 'http://192.168.1.155/'+query
+	
+	req = urllib.request.Request(url)
+	r = urllib.request.urlopen(req).read()
+	cont = json.loads(r.decode('utf-8'))
+
+	#transformando json a una lista
+	data = []
+	#atributos que se extraen desde el json
+	atr1 = 'ceremony'
+	atr2 = 'year'
+	atr3 = 'category'
+	atr4 = 'outcome'
+	atr5 = 'original_languaje'
+	for elem in cont:
+		data.append([elem[atr1], elem[atr2], elem[atr3], elem[atr4], elem[atr5]])
+
+	#tiempo respuesta de consulta	
 	t_resp = -1000
-	context = {'data': data,'query':query, 't_resp':t_resp}
+
+	#datos que se envían al html
+	context = {'data': data,'query':query, 't_resp':t_resp, 'test':cont}
 	return render(request,"search.html",context)
